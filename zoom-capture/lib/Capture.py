0d8c2bb6cd37bfb383faf67bcf8033a9bd480cb5
DiceR011 <touhu0305@gmail.com>
2024-09-19 09:54:08 +0900

    作成したディレクトリにスクショを保存できるようになった

--- zoom-capture/lib/Capture.py
+++ zoom-capture/lib/Capture.py
@@ -7,6 +7,9 @@ def find_zoom_window():
     zoom_windows = [win for win in gw.getAllTitles() if 'Zoom ミーティング' in win in win]
     if zoom_windows:
         return gw.getWindowsWithTitle(zoom_windows[0])[0]
+    else:
+        print("Zoom window not found!")
+        exit()
 
 def capture_window(window):
     window.activate()
--- zoom-capture/main.py
+++ zoom-capture/main.py
@@ -1,11 +1,21 @@
 from lib import capture, operate_dir
-import os
+import datetime,time
 
-Zoom = capture.find_zoom_window()
-Screenshot = capture.capture_window(Zoom)
 
-print(f"Caputured: {Screenshot}")
-path = operate_dir.daily_dir()
+while True:
+    Zoom = capture.find_zoom_window()
+    Screenshot = capture.capture_window(Zoom)
 
-Screenshot.save()
+    print(f"Caputured: {Screenshot}")
 
+    path = operate_dir.daily_dir()
+    date = datetime.datetime.now()
+
+    formatted_date = date.strftime("%Y-%m-%dT%H_%M_%S")
+
+
+    file_path = f"{path}\\{formatted_date}.png"
+    Screenshot.save(file_path)
+
+    print(f"Saved screenshot to: {file_path}")
+    time.sleep(10)
