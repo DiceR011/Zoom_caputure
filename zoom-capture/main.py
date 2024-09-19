172a25a11772465047d18636338b3546d323fdd9
DiceR011 <touhu0305@gmail.com>
2024-09-19 11:17:17 +0900

    時間間隔の設定が可能

--- zoom-capture/main.py
+++ zoom-capture/main.py
@@ -1,6 +1,9 @@
 from lib import capture, operate_dir
 import datetime,time
 
+print("スクショ間隔を秒数で設定してください (5分の場合は'300'と入力)")
+loop_term = int(input())
+
 
 while True:
     Zoom = capture.find_zoom_window()
@@ -18,4 +21,4 @@ while True:
     Screenshot.save(file_path)
 
     print(f"Saved screenshot to: {file_path}")
-    time.sleep(10)
+    time.sleep(loop_term)
