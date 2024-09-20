578019c9073df9ab32d81599dc3af8ec2131a2dc
DiceR011 <touhu0305@gmail.com>
2024-09-20 12:53:25 +0900

    フォルダをzip圧縮してくれる機能を追加

--- zoom-capture/lib/operate_dir.py
+++ zoom-capture/lib/operate_dir.py
@@ -1,4 +1,4 @@
-import os
+import os,zipfile
 import datetime as dt
 
 def daily_dir():
@@ -10,3 +10,13 @@ def daily_dir():
         os.mkdir(create_directory)
     
     return create_directory
+
+def zip_folder(folder_path, output_zip_path):
+    """フォルダをZIP圧縮する関数"""
+    with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                # 圧縮ファイルにフォルダ構造を保持して追加
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, folder_path))
+    print(f"Folder '{folder_path}' has been zipped as '{output_zip_path}'")
---
+++ zoom-capture/uploader.py
@@ -0,0 +1,24 @@
+import os, logging
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from dotenv import load_dotenv
+from lib import operate_dir
+import datetime as dt
+
+#デバックレベルのログを出力
+logging.basicConfig(level=logging.DEBUG)
+
+
+load_dotenv(verbose=True)
+
+slack_token = os.getenv("SLACK_TOKEN")
+slack_channel = os.getenv("CHANNEL_ID")
+client = WebClient(token=slack_token)
+
+daily_path = operate_dir.daily_dir()
+dt_now = dt.datetime.now()
+yyyymmdd = dt_now.strftime('%y%m%d')
+zip_daily_path = os.getcwd() + '\\' + 'Zip_Screenshot' + '\\' + yyyymmdd + '.zip'
+
+operate_dir.zip_folder(daily_path, zip_daily_path)
+
